b13ed7ec44dbdb2c17af492e578d2906d8da51e3
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-03-31 22:18:50 -0400

    Try running JavaScript without needing HTML file

---
+++ js-without-html/js-test.js
@@ -0,0 +1,6 @@
+/*
+Note: Here, I'm also trying out testing JS code directly in VS Code.
+    - You can press the Run button on the top-right.
+    - You can use the terminal with Node.js - just use:
+      >> node <filename.js>
+*/
