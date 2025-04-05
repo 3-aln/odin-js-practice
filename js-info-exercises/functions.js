234ae98fd398445342002d34416d3bdceb8a8daf
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-05 14:19:24 -0400

    Solve exercise

--- js-info-exercises/functions.js
+++ js-info-exercises/functions.js
@@ -11,8 +11,16 @@ function checkAge(age) {
 // Rewrite without if, in a single line:
 
 // 1. Using question mark operator:
+function checkAge1(age) {
+  return age > 18 ? true : confirm('Did parents allow you?');
+}
 
 // 2. Using OR ||
+function checkAge2(age) {
+  return age > 18 || confirm('Did parents allow you?');
+}
 
 // Testing
-console.log(checkAge(20));
+// console.log(checkAge(16));
+// console.log(checkAge1(19));
+console.log(checkAge2(18));
