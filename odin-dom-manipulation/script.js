8e3780b688094e663e64b3eafa68284c8e78f1c2
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-15 23:47:06 -0400

    Add red paragraph using JS

--- odin-dom-manipulation/script.js
+++ odin-dom-manipulation/script.js
@@ -5,3 +5,10 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+// Add <p> with red text that says "Hey I'm red!"
+const redPara = document.createElement("p");
+redPara.style.color = "red";
+redPara.textContent = "Hey I'm red!";
+
+container.appendChild(redPara);
