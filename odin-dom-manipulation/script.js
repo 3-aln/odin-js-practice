3cf6106a5d1dcd88fdd212809015ee39782f2462
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-15 23:50:39 -0400

    Add blue header using JS

--- odin-dom-manipulation/script.js
+++ odin-dom-manipulation/script.js
@@ -12,3 +12,10 @@ redPara.style.color = "red";
 redPara.textContent = "Hey I'm red!";
 
 container.appendChild(redPara);
+
+// Add an <h3> with blue text that says "I'm a blue h3!"
+const blueH3 = document.createElement("h3");
+blueH3.style.color = "blue";
+blueH3.textContent = "I'm a blue H3!";
+
+container.appendChild(blueH3);
