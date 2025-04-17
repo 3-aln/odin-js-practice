35bdd6da9caa3ae1644e96f251b40d4bc9dbfc16
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-16 20:52:05 -0400

    Add pink div with interior elements

--- odin-dom-manipulation/script.js
+++ odin-dom-manipulation/script.js
@@ -19,3 +19,21 @@ blueH3.style.color = "blue";
 blueH3.textContent = "I'm a blue H3!";
 
 container.appendChild(blueH3);
+
+// Add a <div> with a black border and pink background color
+const div = document.createElement("div");
+div.style.borderStyle = "solid";
+div.style.borderColor = "black";
+div.style.backgroundColor = "pink";
+
+// In the <div>, add another <h1> that says "I'm in a div"
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+div.appendChild(h1);
+
+// In the <div> add <p> that says "ME TOO!"
+const divPara = document.createElement("p");
+divPara.textContent = "ME TOO!";
+div.appendChild(divPara);
+
+container.appendChild(div);
