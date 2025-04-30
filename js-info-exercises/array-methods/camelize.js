7f576cbec736a6ef56a179cd107870bfcd58d5f6
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-29 20:19:10 -0400

    Complete translate border-left-width […] exercise

---
+++ js-info-exercises/array-methods/camelize.js
@@ -0,0 +1,28 @@
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
+
+function camelize(str) {
+  // Create array of strings, split by '-'
+  splitStrs = str.split("-");
+
+  // Remove first item unchanged and store it
+  first = splitStrs.shift();
+
+  // Capitalize the remaining strings in the array
+  upperStrs = splitStrs.map((item) => {
+    return item.charAt(0).toUpperCase() + item.slice(1);
+  });
+
+  return first + upperStrs.join("");
+}
+
+// Website solution
+function camelizeSolution(str) {
+  return str
+    .split('-')
+    .map(
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join('');
+}
